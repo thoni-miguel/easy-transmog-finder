ad5cdb011cd6bc2e53f3c52641f55929f2f49591
thoni-miguel <thonimigueldev@gmail.com>
2024-04-19 19:11:45 -0300

    First try

--- main.lua
+++ main.lua
@@ -1,1 +1,24 @@
-message("Hello Addon World!")
+wowheadUrl = "https://www.wowhead.com/wow/retail"
+
+-- Create a frame to handle the keybind
+local frame = CreateFrame("Frame", "MyKeybindFrame", UIParent, "SecureHandlerBaseTemplate")
+
+
+-- Register the keybind
+frame:SetAttribute("_onstate-keyup", [[
+    if newstate == "up" then
+        -- Call your specific function when the keybind is pressed
+        MyFunction()
+    end
+]])
+
+-- Set up the keybind
+SetBinding(frame, "keyup", "[mod:shift] up; none")
+
+-- Function to be called when the keybind is pressed
+function MyFunction()
+    -- Your specific function code goes here
+    print("Keybind pressed!")
+    local item = C_Cursor.GetCursorItem
+    print(item)
+end
